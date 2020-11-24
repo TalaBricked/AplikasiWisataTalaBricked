7510845e7fbe3b934b7218d5b144290ce14bfa54
alisiass <alisiasilverstone101@gmail.com>
2020-11-24 14:41:51 +0700

    new

--- app/src/main/java/com/example/aplikasiwisatatalabricked/SplashScreen.java
+++ app/src/main/java/com/example/aplikasiwisatatalabricked/SplashScreen.java
@@ -1,7 +1,8 @@
 package com.example.aplikasiwisatatalabricked;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread() {
+            public <SplashScreen> void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreen.this,
+                            login.Login.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
     }
-}
+    }
